d849ec74a4a118e97bc2bd616716bf8bb1e4a949
Raúl Astorga <r.astorga12@gmail.com>
2026-09-03 18:18:25 -0700

    se actualiza libreria del mapa

--- src/js/script.js
+++ src/js/script.js
@@ -297,9 +297,8 @@ function initMap() {
     if (!mapEl) return;
     
     myMap = L.map('map').setView([25.0, -107.5], 7);
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; OpenStreetMap & CARTO',
-        subdomains: 'abcd',
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         maxZoom: 19
     }).addTo(myMap);
 
